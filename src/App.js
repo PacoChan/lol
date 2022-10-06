421b570a733c3b54748f2fdd9b3ccecb04df0832
PacoChan <109855305+PacoChan@users.noreply.github.com>
2022-10-06 19:16:10 +0800

    change css

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import { champions } from "./data/champions";
 
 function App() {
-	return (
-		<div className="App">
-			{champions.map((champion) => (
-				<ChampionCard champion={champion} key={champion.id} />
-			))}
-		</div>
-	);
+  return (
+    <div className="App">
+      <div>
+        {champions.map((champion) => (
+          <ChampionCard champion={champion} key={champion.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
