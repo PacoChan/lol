a90c6fcd3260ff8017af6135775a19062a18d367
MrGGTo <56320644+MrGGTo@users.noreply.github.com>
2022-10-30 01:06:48 +0800

    Code Refactor

--- src/App.js
+++ src/App.js
@@ -4,64 +4,70 @@ import { champions, getRoles } from "./data/champions";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState();
   const [champs, setChamps] = useState([]);
   const [selectedRoles, setSelectedRoles] = useState([]);
   const currentChampion = champs[index];
   const roles = getRoles();
 
   useEffect(() => {
-    const filteredChampions = champions.filter((champion) => {
-      for (let index = 0; index < champion.roles.length; index++) {
-        return selectedRoles.includes(champion.roles[index]);
-      }
-    });
-
-    setChamps(filteredChampions);
+    if (selectedRoles.length > 0) {
+      const filteredChampions = champions.filter((champion) => {
+        for (let index = 0; index < champion.roles.length; index++) {
+          return selectedRoles.includes(champion.roles[index]);
+        }
+      });
+      setChamps(filteredChampions);
+    } else {
+      setChamps(champions);
+    }
   }, [selectedRoles]);
 
   return (
     <div className="App">
-      <div className="tittle">
-        <h1>NO IDEA WHICH CHAMPION TO PICK?</h1>
-        <p>
-          With more than 140 champions, you’ll find the perfect match for your
-          playstyle. Master one, or master them all.
-        </p>
+      <div className="intro">
+        <div className="tittle">
+          <h1>NO IDEA WHICH CHAMPION TO PICK?</h1>
+          <p>
+            With more than 140 champions, you’ll find the perfect match for your playstyle. Master
+            one, or master them all.
+          </p>
 
-        {selectedRoles}
-        <button
-          onClick={() => {
-            setIndex(Math.floor(Math.random() * champs.length));
-          }}
-        >
-          FIND ONE
-        </button>
+          {selectedRoles}
+          <button
+            onClick={() => {
+              setIndex(Math.floor(Math.random() * champs.length));
+            }}
+          >
+            FIND ONE
+          </button>
+        </div>
+        <div className="filterRoles">
+          {roles.map((role, index) => {
+            return (
+              <div key={role}>
+                <input
+                  type={"checkbox"}
+                  onChange={() => {
+                    if (selectedRoles.includes(role)) {
+                      setSelectedRoles(selectedRoles.filter((r) => r !== role));
+                    } else {
+                      setSelectedRoles([...selectedRoles, role]);
+                    }
+                  }}
+                />
+                {role}
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <div className="filterRoles">
-        {roles.map((role, index) => {
-          return (
-            <div key={role}>
-              <input
-                type={"checkbox"}
-                onChange={() => {
-                  if (selectedRoles.includes(role)) {
-                    setSelectedRoles(selectedRoles.filter((r) => r !== role));
-                  } else {
-                    setSelectedRoles([...selectedRoles, role]);
-                  }
-                }}
-              />
-              {role}
-            </div>
-          );
-        })}
-      </div>
-
       <div className="appChampSty">
-        <ChampionCard champion={currentChampion} />
+        {index == undefined ? <div>sss</div> : <ChampionCard champion={currentChampion} />}
+        {/* <ChampionCard champion={currentChampion} /> */}
       </div>
 
+      {/* TODO: Refencence, css: flex-wrap */}
       {/* <div className="champions">
         {champions.map((champion) => (
           <ChampionCard champion={champion} key={champion.id} />
--- src/components/RatingBar/index.js
+++ src/components/RatingBar/index.js
@@ -1,18 +1,17 @@
 import React from "react";
 
-export default function RatingBar({ title = "", value = 0, maxValue = 10 }) {
+export default function RatingBar({ title = "", color = "#778BD6", value = 0, maxValue = 10 }) {
   return (
     <div>
       <span>
         <label style={{ color: "white" }}>{title}</label>
-        if (({title} = "title"))
         {
           <div style={{ backgroundColor: "white" }}>
             <div
               style={{
                 height: 8,
                 width: (value / maxValue) * 100 + "%",
-                backgroundColor: " red",
+                backgroundColor: color,
               }}
             ></div>
           </div>
